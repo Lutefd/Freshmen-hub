ab0f0f3f665f13dd09a3d9b93e240b63722aedb0
Lutefd <lfaddourado@gmail.com>
2022-04-25 14:06:17 -0300

    FAQ section done

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ window.addEventListener('scroll', () => {
     .querySelector('nav')
     .classList.toggle('window-scroll', window.scrollY > 0);
 });
+
+// show/hide faqs answers
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+    //changing the icon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-plus') {
+      icon.className = 'uil uil-minus';
+    } else {
+      icon.className = 'uil uil-plus';
+    }
+  });
+});
